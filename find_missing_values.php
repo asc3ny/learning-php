64d6f83dcb0aa5194b125ebbc748b84f413f6093
Rosh1ck <58085779+Rosh1ck@users.noreply.github.com>
2019-12-03 13:24:36 +0300

    Create find_missing_values.php

---
+++ find_missing_values.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+* @require PHP7 
+*/
+
+function findMissingValues(array $inputArray): array
+{
+    $resultArray = [];
+    for ($number = min($inputArray); $number < max($inputArray); $number++)
+    {
+        if(!in_array($number, $inputArray))
+        {
+            array_push($resultArray, $number);
+        }
+    }
+    return $resultArray;
+}
+
+print_r(findMissingValues($inputArray = [-2,-1,3,4,5]));
+print_r(findMissingValues($inputArray = [2,-1,3]));
+print_r(findMissingValues($inputArray = [1,2,3]));
