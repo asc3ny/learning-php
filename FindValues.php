a008331e19846eb78a38d6df47cea4ee0045e9fc
Rosh1ck <58085779+Rosh1ck@users.noreply.github.com>
2019-12-03 22:30:52 +0300

    Update name and variables

---
+++ FindValues.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+* @require PHP7
+*/
+
+class FindValues
+{
+    public function findMissingValues(array $inputArray): array
+    {
+        $resultArray = [];
+        for ($number = min($inputArray); $number < max($inputArray); $number++)
+        {
+            if(!in_array($number, $inputArray))
+            {
+                array_push($resultArray, $number);
+            }
+        }
+        return $resultArray;
+    }
+}
+
+$findValues = new FindValues;
+
+print_r($findValues->findMissingValues($inputArray = [-2, -1, 3, 4, 5]));
+print_r($findValues->findMissingValues($inputArray = [0, 2, -1, 3]));
+print_r($findValues->findMissingValues($inputArray = [1, 2, 3]));
