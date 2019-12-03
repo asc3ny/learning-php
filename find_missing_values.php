f39cff1993f57cad8c5ac1d8fb36c4f8fc8d4a51
Rosh1ck <58085779+Rosh1ck@users.noreply.github.com>
2019-12-03 16:50:51 +0300

    Creating class FindValues

--- find_missing_values.php
+++ find_missing_values.php
@@ -4,19 +4,24 @@
 * @require PHP7 
 */
 
-function findMissingValues(array $inputArray): array
+class FindValues 
 {
-    $resultArray = [];
-    for ($number = min($inputArray); $number < max($inputArray); $number++)
-    {
-        if(!in_array($number, $inputArray))
-        {
-            array_push($resultArray, $number);
-        }
-    }
-    return $resultArray;
+	public function findMissingValues(array $inputArray): array
+	{
+		$resultArray = [];
+		for ($number = min($inputArray); $number < max($inputArray); $number++)
+		{
+			if(!in_array($number, $inputArray))
+			{
+				array_push($resultArray, $number);
+			}
+		}
+		return $resultArray;
+	}
 }
 
-print_r(findMissingValues($inputArray = [-2, -1, 3, 4, 5]));
-print_r(findMissingValues($inputArray = [0, 2, -1, 3]));
-print_r(findMissingValues($inputArray = [1, 2, 3]));
+$missingValues = new FindValues;
+
+print_r($missingValues->findMissingValues($inputArray = [-2, -1, 3, 4, 5]));
+print_r($missingValues->findMissingValues($inputArray = [0, 2, -1, 3]));
+print_r($missingValues->findMissingValues($inputArray = [1, 2, 3]));
